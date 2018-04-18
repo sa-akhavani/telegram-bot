c52086675b1e1bb71c7f154bd12f35e8f3bbb052
Seyyed Ali Akhavani <sa.akhavani@gmail.com>
2018-04-18 23:54:54 +0430

    Remove Hooman Feature

--- app.js
+++ app.js
@@ -8,11 +8,7 @@ bot.on('text', (msg) => {
   let text = msg.text;
   let chatId = msg.chat.id;
   if (text.search('بسه دیگه') !== -1) {
-    if (msg.from.id === 86530693) {
-      bot.sendMessage(chatId, 'هومن جان قار نکش!');
-    } else {
-      bot.forwardMessage(chatId, -1001384114403, 3);
-    }
+    bot.forwardMessage(chatId, -1001384114403, 3);
   }
 });
 
